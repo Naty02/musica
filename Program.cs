05b23b14e6203cbd36b72f0e7f3b01aa1ca6ca42
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-08-30 01:35:36 -0300

    resolução do exercício musica

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("");
+            Console.WriteLine("Snow Patrol - Chasing Cars");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("We'll do it all, everything");
+            Console.WriteLine("On our own");
+            Console.WriteLine("We don't need, anything");
+            Console.WriteLine("Or anyone");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("If I lay here");
+            Console.WriteLine("If I just lay here");
+            Console.WriteLine("Would you lay with me and just forget the world?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("I don't quite know");
+            Console.WriteLine("How to say");
+            Console.WriteLine("How I feel");
+            Console.WriteLine("");
+            Console.WriteLine("Those three words");
+            Console.WriteLine("Are said too much");
+            Console.WriteLine("They're not enough");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("If I lay here");
+            Console.WriteLine("If I just lay here");
+            Console.WriteLine("Would you lay with me and just forget the world?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("Forget what we're told");
+            Console.WriteLine("Before we get too old");
+            Console.WriteLine("Show me a garden that's bursting into life");
+            Console.WriteLine("");
+            Console.WriteLine("Let's waste time");
+            Console.WriteLine("Chasing cars");
+            Console.WriteLine("Around our heads");
+            Console.WriteLine("");
+            Console.WriteLine("I need your grace");
+            Console.WriteLine("To remind me");
+            Console.WriteLine("To find my own");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("If I lay here");
+            Console.WriteLine("If I just lay here");
+            Console.WriteLine("Would you lay with me and just forget the world?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Forget what we're told");
+            Console.WriteLine("Before we get too old");
+            Console.WriteLine("Show me a garden that's bursting into life");
+            Console.WriteLine("");
+            Console.WriteLine("All that I am");
+            Console.WriteLine("All that I ever was");
+            Console.WriteLine("Is here in your perfect eyes");
+            Console.WriteLine("They're all I can see");
+            Console.WriteLine("");
+            Console.WriteLine("I don't know where");
+            Console.WriteLine("Confused about how as well");
+            Console.WriteLine("Just know that these things will never");
+            Console.WriteLine("Change for us at all");
+            Console.WriteLine("");            
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("If I lay here");
+            Console.WriteLine("If I just lay here");
+            Console.WriteLine("Would you lay with me and just forget the world?");
+            Console.ResetColor();
+
+        }
+    }
+}
